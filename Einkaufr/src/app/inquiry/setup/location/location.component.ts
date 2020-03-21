c2951c2a0418f15aa71ed6e5473d0cced3c26569
phdiss <phdiss@outlook.com>
2020-03-21 18:01:27 +0100

    Update search list on keydown

--- Einkaufr/src/app/inquiry/setup/location/location.component.ts
+++ Einkaufr/src/app/inquiry/setup/location/location.component.ts
@@ -1,22 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LocationService, SearchAddressResponse } from "../../../services/location.service";
+import { Subject } from "rxjs";
+import { catchError, debounceTime, distinctUntilChanged, filter, switchMap } from "rxjs/operators";
 
 @Component({
   selector: 'app-location',
   templateUrl: './location.component.html',
   styleUrls: ['./location.component.css']
 })
-export class LocationComponent {
+export class LocationComponent implements OnInit {
 
   constructor(
     private locationService: LocationService
   ) {
   }
 
+  private searchUpdated: Subject<string> = new Subject<string>();
+
   public searchResults: SearchAddressResponse | null = null;
 
-  onChange(event: any) {
-    this.locationService.coordinatesFromSearch(event.target.value)
+  onSearchKeyUp(event: any) {
+    const value: string = event.target.value;
+    this.searchUpdated.next(value);
+  }
+
+  ngOnInit(): void {
+    this.searchUpdated
+      .pipe(
+        debounceTime(200),
+        distinctUntilChanged(),
+        filter(v => v != ''),
+        switchMap(v => this.locationService.coordinatesFromSearch(v)),
+        // log error, but don't end pipe
+        catchError((e, obs) => {console.error(e); return obs;}),
+      )
       .subscribe(value => this.searchResults = value);
   }
 }

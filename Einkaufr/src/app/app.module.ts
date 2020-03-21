b918b4aa974e193f433181802a01694562f792e9
wirkus <Lea-Helena.Wirkus@adesso.de>
2020-03-21 11:42:39 +0100

    basic pages, paths

--- Einkaufr/src/app/app.module.ts
+++ Einkaufr/src/app/app.module.ts
@@ -3,12 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { InputComponent } from './helper/input/input.component';
+import { OverviewComponent } from './helper/overview/overview.component';
+import { DetailviewComponent } from './helper/detailview/detailview.component';
 
+const appRoutes: Routes =[
+  {path: 'home', component: HomeComponent},
+  {path: 'helper/input', component: InputComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', component: HomeComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    InputComponent,
+    OverviewComponent,
+    DetailviewComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     AppRoutingModule
   ],
